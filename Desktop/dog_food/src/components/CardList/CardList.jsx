713f218017e2f4b32da9d35cfe41c4081bc6c8aa
Mel-ok <animeshnitsa264@mail.ru>
2023-01-08 22:08:03 +0300

    Dog_food основа

--- Desktop/dog_food/src/components/CardList/CardList.jsx
+++ Desktop/dog_food/src/components/CardList/CardList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './style.css';
-// import cards from './data.json';
+// import cards from '../data.json';
 import Card from '../Card/Card';
 
-const CardList = (cards) => {
-    return (<div className="cards">
-        {cards.map((card, i) => (
-        <Card key={i} {...card} />
-        ))}
-    </div>
+const CardList = ({cards}) => {
+    return (
+        <div className="cards">
+            {cards?.map((card, i) => (
+            <Card key={i} {...card} />
+            ))}
+        </div>
     );
 };
 
